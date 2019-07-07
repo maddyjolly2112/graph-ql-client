59e3cb286d3846ce41b9877294067161464c37d3
Mandeep Singh <maddyjolly2112@gmail.com>
2019-07-07 11:43:43 +0200

    updated for queries

--- src/App.js
+++ src/App.js
@@ -10,30 +10,68 @@ const axiosGitHubGraphQL = axios.create({
   },
 });
 
+
+const GRAPHQL_QUERY = `
+  {
+    viewer { 
+      login
+      name
+    }
+  }
+`;
+
 const title = 'GraphQL Client';
 
 class App extends Component {
+
+  state = {
+    path: 'maddyjolly2112',
+    errors: null,
+  };
+
+  componentDidMount() {
+    this.getDataFromGithub();
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.getDataFromGithub();
+  };
+
+  getDataFromGithub = () => {
+    axiosGitHubGraphQL
+      .post('', { query: GRAPHQL_QUERY })
+      .then(result =>
+        this.setState(() => ({
+          errors: result.data.errors,
+        })),
+      );
+  };
+no
   render() {
+    const { path } = this.state;
     return (
       <div>
         <h1>{title}</h1>
 
         <form onSubmit={this.onSubmit}>
           <label htmlFor="url">
-            Show open issues for https://github.com/
+            https://github.com/
           </label>
           <input
             id="url"
             type="text"
+            value={path}
             onChange={this.onChange}
             style={{ width: '300px' }}
           />
           <button type="submit">Search</button>
         </form>
 
-        <hr />
-
-        {/* Here comes the result! */}
       </div>
     );
   }
